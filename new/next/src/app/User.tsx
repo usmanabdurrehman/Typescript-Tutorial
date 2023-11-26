d1566be1f9b7f23021ac1593c0b22655a7621f5f
Usman Abdur Rehman <saidskit@outlook.com>
2023-11-26 20:27:39 +0500

    Pnpm Tutorial Code

--- new/next/src/app/User.tsx
+++
@@ -1,6 +0,0 @@
-const User = (props: { "a:b": string }) => {
-    return <div>{props["a:b"]}</div>;
-  };
-  
-const userComp = <User a : b={"Alex"} />;
-  
---
+++ pnpm-project/index.js
@@ -0,0 +1,3 @@
+import debug from "debug";
+
+console.log(debug);
